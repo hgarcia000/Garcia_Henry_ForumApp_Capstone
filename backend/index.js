70d922666b4bccded96046e5f2ffadd21d31229e
hgarcia000 <hgarcia8889@gmail.com>
2025-07-14 13:21:33 -0400

    Enabled CORS for backend server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRouter from './routes/userRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 await mongoose.connect(process.env.MONGO_URI);
 console.log(Date(), ' : MongoDB Connected!');
