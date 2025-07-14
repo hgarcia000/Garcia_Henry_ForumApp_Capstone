052c924796f119e2d3ab98715afb813ffc0c20bd
hgarcia000 <hgarcia8889@gmail.com>
2025-07-14 11:35:43 -0400

    Establish backend server connection to the database.

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config'
+import mongoose from 'mongoose';
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+await mongoose.connect(process.env.MONGO_URI);
+console.log(Date(), ' : MongoDB Connected!')
+
+app.listen(PORT, () => {
+    console.log('Server running on port', PORT);
+});
