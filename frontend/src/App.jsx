6e4d3a901861f7648e3257ba11667b73a153ae48
hgarcia000 <hgarcia8889@gmail.com>
2025-07-18 13:39:57 -0400

    Changed Post route path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Route path='/signup' element={<Signup />} />
       <Route path='/login' element={<Login />} />
       <Route path='/profile' element={<Profile />} />
-      <Route path='/post' element={<Post />} />
+      <Route path='/post/:id' element={<Post />} />
     </Routes>
     // <>
     //   <div>
