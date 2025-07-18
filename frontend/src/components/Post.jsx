c3896ca7d9917de18d476730cdbf5ea7cc0f6a54
hgarcia000 <hgarcia8889@gmail.com>
2025-07-18 13:59:17 -0400

    added useSate to the Post component.

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,7 +1,23 @@
+import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
+import { useState } from "react";
 
 
 function Post() {
+
+    const paramsId = useParams().id;
+    const [post,setPost] = useState({
+        category: "",
+        title: "",
+        postedBy: "",
+        postedAt: "",
+        body: "",
+        isLocked: false,
+        editedAt: "",
+        comments: []
+    });
+
+    const URL = `${import.meta.env.VITE_BASEURL}/api/posts/${paramsId}`;
 
     return (
         <>
