497483256c21a9384149532c6843ab92e60fccdd
hgarcia000 <hgarcia8889@gmail.com>
2025-07-21 11:23:35 -0400

    Provided current user context to the App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,21 @@ import Signup from './components/SignUp'
 import Login from './components/Login'
 import Profile from './components/Profile'
 import Post from './components/Post'
+import { UserContext } from './UserContext.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentUser, setCurrentUser] = useState(null)
 
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/post/:id' element={<Post />} />
-    </Routes>
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/post/:id' element={<Post />} />
+      </Routes>
+    </UserContext.Provider>
     // <>
     //   <div>
     //     <a href="https://vite.dev" target="_blank">
