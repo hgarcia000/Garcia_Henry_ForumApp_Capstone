a926db7870b1932a482b2fea81a6c61db6ef14ae
hgarcia000 <hgarcia8889@gmail.com>
2025-07-23 15:08:10 -0400

    Showed comments on the Post Component.

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -5,6 +5,9 @@ import axios from "axios";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import Fab from "@mui/material/Fab";
+import AddIcon from '@mui/icons-material/Add';
 
 
 function Post() {
@@ -39,7 +42,7 @@ function Post() {
 
     const PostJSX = () => {
         return (
-            <Box width={950}  sx={{ border: '2px solid #111', borderRadius: '3px', bgcolor: '#555' }}>
+            <Box width={950} marginBottom={2}  sx={{ border: '2px solid #111', borderRadius: '3px', bgcolor: '#555' }}>
                 <Typography variant="h3">{post.title}</Typography>
                 <Link to={`/user/${post.postedBy}`}><Typography color="primary" sx={{":hover": {color: "#535bf2"}}} marginLeft={1} fontWeight={'bold'} textAlign={'justify'} ><i> {post.postedBy} </i></Typography></Link>
                 <Divider />
@@ -51,10 +54,25 @@ function Post() {
         );
     };
 
+    const commentJSX = post.comments.map(element => {
+        return(
+        <Box width={950}  sx={{ border: '2px solid #111', borderRadius: '3px', bgcolor: '#555' }}>
+            <Link to={`/user/${element?.postedBy}`}><Typography color="primary" sx={{":hover": {color: "#535bf2"}}} marginLeft={1} fontWeight={'bold'} textAlign={'justify'} ><i> {element?.postedBy} </i></Typography></Link>
+                <Divider />
+                <Typography padding={2} minHeight={200} sx={{textAlign:'justify', bgcolor: '#333' }}>{element?.body}</Typography>
+                <Divider />
+                <Typography paddingRight={2} paddingBottom={1} sx={{textAlign:'right',color: 'gray', bgcolor: '#333', fontSize: '11pt', fontStyle: 'italic' }}>Posted at {element?.postedAt}. {element?.editedAt ? ` Last edited at ${element?.editedAt}.` : ''}</Typography> 
+        </Box>
+        )
+    });
+
     return (
         <>
             <NavBar />
+            <Button sx={{marginBottom: 1}} variant="outlined"> <AddIcon /> Reply to Thread</Button>
             <PostJSX />
+            {post.comments.length > 0 ? commentJSX : ''}
+            <Button sx={{marginTop: 1}} variant="outlined" s> <AddIcon /> Reply to Thread</Button>
         </>
     )
 }
